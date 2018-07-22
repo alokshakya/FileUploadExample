637d76079fcf1be5889c79dca8409d88ef0a498e
alokshakya <alokshakyacode@gmail.com>
2018-07-22 12:22:08 +0530

    signup route done and login is being set

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,61 @@
 var express = require('express');
+const bodyParser = require('body-parser');
+var User = require('./models/user');
+
 var router = express.Router();
+router.use(bodyParser.json());
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/signup', function( req, res, next){
+  User.findOne({username: req.body.username})
+  .then((user) =>{
+    if(user !=null){
+      var err = new Error('User '+ req.body.username + ' already exists!');
+      err.status = 403;
+      next(err);
+    }
+    else{
+      return User.create({
+        username: req.body.username,
+        password: req.body.password
+      });
+    }
+  })
+  .then((user) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type','application/json');
+    res.json({ status: 'Registration Successful', user: user});
+  }, (err) => next(err))
+  .catch((err) => next(err));
+});
+
+router.post('/login', (req, res, next) => {
+    if (!req.session.user) {
+      var authHeader = req.headers.authorization;
+      if (!authHeader) {
+          var err = new Error('You are not authenticated!');
+          res.setHeader('WWW-Authenticate', 'Basic');                        
+          err.status = 401;
+          next(err);
+          return;
+      }
+      var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+      var user = auth[0];
+      var pass = auth[1];
+      if (user == 'admin' && pass == 'password') {
+          req.session.user = 'admin';
+          next(); // authorized
+      } else {
+          var err = new Error('You are not authenticated!');
+          res.setHeader('WWW-Authenticate', 'Basic');
+          err.status = 401;
+          next(err);
+      }
+  }
 });
 
 module.exports = router;
