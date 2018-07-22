726bde97ad13089100f36930f77d73ec55f7d097
alokshakya <alokshakyacode@gmail.com>
2018-07-22 18:21:18 +0530

    User Schema modified for passport-local-mongoose

--- models/user.js
+++ models/user.js
@@ -1,21 +1,14 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-
+var passportLocalMongoose = require('passport-local-mongoose');
 var User = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
     admin: {
         type: Boolean,
         default: false
     }
 
 });
+
+User.plugin(passportLocalMongoose); // this will add password hash
 
 module.exports = mongoose.model('User', User);
