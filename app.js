2932e4e0dfbb404f525a04918286c59e18e67010
alokshakya <alokshakyacode@gmail.com>
2018-07-23 13:07:10 +0530

    app.js modified for passport js

--- app.js
+++ app.js
@@ -23,6 +23,8 @@ app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser('12345-67890-09876-54321'));
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 app.use(session({
     name: 'session-id',
@@ -31,25 +33,22 @@ app.use(session({
     resave: false,
     store: new FileStore()
   }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
   function auth (req, res, next) {
       console.log(req.session);
   
-      if (!req.session.user) {
+      if (!req.user) {
               var err = new Error('You are not authenticated!');                      
               err.status = 403;
               return next(err);   
       }
       else {
-          if (req.session.user === 'authenticated') {
-              next();
-          }
-          else {
-              var err = new Error('You are not authenticated!');
-              err.status = 403;
-              return next(err);
-          }
+          next();
       }
   }
   
