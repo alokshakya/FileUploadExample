62a392d848c62e9384cd54b898f371e02d9e9143
alokshakya <alokshakyacode@gmail.com>
2018-07-26 03:05:58 +0530

    File Upload

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+const uploadRouter = require('./routes/uploadRouter');
 
 var app = express();
 
@@ -49,6 +50,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
+app.use('/imageUpload',uploadRouter);
 //for making full rest api
 const mongoose = require('mongoose');
 
