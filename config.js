cc752d45b8b2342d4aee9545cb4c212163d7838e
alokshakya <alokshakyacode@gmail.com>
2018-07-29 23:29:46 +0530

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId:'329999474206572',
+        clientSecret:'56503eed5402bd290d95be895f5ca89d'
+    }
 }
