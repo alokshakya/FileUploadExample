369cea51994bebe42644d1b662361cf7b4c7aa7f
alokshakya <alokshakyacode@gmail.com>
2018-08-03 17:45:07 +0530

    final Schema of Favorites Dish

--- models/favorite.js
+++ models/favorite.js
@@ -1,17 +1,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-var favoriteDishSchema = new Schema({
-    _id:  {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Dish' //Dishes can't be used as it is exported not schema
-    }
-});
 var favoriteSchema = new Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
     },
-    dishes:[favoriteDishSchema] 
+    dishes:[
+            {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Dish'
+        }
+    ] 
     }
 , {
     timestamps: true
