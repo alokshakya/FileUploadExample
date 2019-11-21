2660d4ce4eb30869f1e1aef4081377463cb1a3ed
Alok Shakya <alokshakyacode@gmail.com>
2019-11-21 23:38:55 +0530

    adding other changes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var index = express.Router();
+var path = require('path');
 
 /* GET home page. */
 index.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //res.render('index', { title: 'Express' });
+  res.sendFile(path.join(__dirname, '../public', 'up.html'));
+  //res.sendFile(path.join(__dirname, '../public','up.html'));
+  //res.sendFile(path.join(__dirname, '../public', 'index1.html'));
 });
 
 module.exports = index;
